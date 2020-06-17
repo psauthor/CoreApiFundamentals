df66863bf95a824618e03709a21bcd6ea03ae6ed
Victor Velasquez <victorcop90@gmail.com>
2020-06-17 18:08:33 -0500

    proyect-clean-up

--- src/Data/CampRepository.cs
+++ src/Data/CampRepository.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace CoreCodeCamp.Data
 {
-  public class CampRepository : ICampRepository
+    public class CampRepository : ICampRepository
   {
     private readonly CampContext _context;
     private readonly ILogger<CampRepository> _logger;
