98c8dc6b946056a8f9d2bc5f67da07e2be46ee01
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-20 09:32:57 -0600

    Add Camps Get route

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {  
+        public object Get()
+        {
+            return new { Moniker = "ATL2020", Name = "Atlanta Code Camp" };
+        }
+        
+    }
+}
