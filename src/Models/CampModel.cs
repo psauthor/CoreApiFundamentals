10c9256187893f05bd9ee4f5373c5dc349c86bad
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-20 10:23:27 -0600

    Add CampModel

---
+++ src/Models/CampModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Models
+{
+    public class CampModel
+    {
+        public string Name { get; set; }
+        public string Moniker { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        public int Length { get; set; } = 1;
+    }
+}
