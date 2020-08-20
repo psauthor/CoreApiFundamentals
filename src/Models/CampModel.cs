e2d63c2cef099ffb49b2768a5837587a8657f8ac
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-20 12:25:00 -0600

    Add Location fields to CampModel

--- src/Models/CampModel.cs
+++ src/Models/CampModel.cs
@@ -12,5 +12,13 @@ namespace CoreCodeCamp.Models
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
+        public string LocationVenueName { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationAddress2 { get; set; }
+        public string LocationAddress3 { get; set; }
+        public string LocationCityTown { get; set; }
+        public string LocationStateProvince { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
     }
 }
