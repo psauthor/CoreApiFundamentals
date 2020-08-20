ab1f0985a37a1023cc068e734ee5dae832b0d86e
Jeff Maughan <jeff.maughan@intimetec.com>
2020-08-20 12:58:22 -0600

    Add Speakers to TalkModel

---
+++ src/Models/SpeakerModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CoreCodeCamp.Models
+{
+    public class SpeakerModel
+    {
+        public int SpeakerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Company { get; set; }
+        public string CompanyUrl { get; set; }
+        public string BlogUrl { get; set; }
+        public string Twitter { get; set; }
+        public string GitHub { get; set; }
+    }
+}
--- src/Models/TalkModel.cs
+++ src/Models/TalkModel.cs
@@ -8,8 +8,11 @@ namespace CoreCodeCamp.Models
 {
     public class TalkModel
     {
+        public int TalkId { get; set; }
         public string Title { get; set; }
         public string Abstract { get; set; }
         public int Level { get; set; }
+
+        public SpeakerModel Speaker { get; set; }
     }
 }
